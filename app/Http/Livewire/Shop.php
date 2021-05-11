41efb7475c66895921bd218bc18fc3eb1013d19e
Cizz22 <auliacisatra14@gmail.com>
2021-05-11 14:14:23 +0700

    menambah cart

--- app/Http/Livewire/Shop.php
+++ app/Http/Livewire/Shop.php
@@ -3,13 +3,65 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Product as M_Product;
 
 class Shop extends Component
 {
-
+    public $pajak = "0%";
 
     public function render()
     {
-       
+        $products = M_Product::orderBy('created_at', 'DESC')->get();
+        $id= Auth()->id();
+        $condition = new \Darryldecode\Cart\CartCondition([
+            'name' => 'pajak',
+            'type' => 'tax',
+            'target' => 'total',
+            'value' => $this->pajak,
+            'order' => 1
+        ]);
+
+        \Cart::session($id)->condition($condition);
+        $items = \Cart::session($id)->getContent()->sortBy(function ($cart){
+            return $cart->attributes->get('added_at');
+        });
+
+
+
+        if(\Cart::session($id)->isEmpty()){
+            $cartData = [];
+        }else{
+            foreach ($items as $item) {
+                $cart[] = [
+                    'rowId' => $item->id,
+                    'name' => $item->name,
+                    'qty' => $item->qty,
+                    'price' => $item->price,
+                    'total' => $item->getPriceSum(),
+                ];
+            }
+                $cartData = collect($cart);
+            }
+            
+            $subtotal = \Cart::session($id)->getSubTotal();
+            $total = \Cart::session($id)->getTotal();
+            
+
+            $newCondition = \Cart::session($id)->getCondition('pajak');
+            $pajak = $newCondition->getCalculatedValue($subtotal);
+
+
+            $data = [
+                'sub_total' => $subtotal,
+                'total' => $total,
+                'pajak' => $pajak
+            ];
+
+            return view('livewire.shop', [
+                'products' => $products,
+                'carts' => $cartData,
+                'data' => $data
+            ]);
+        }
     }
-}
+    
--- resources/views/livewire/shop.blade.php
+++ resources/views/livewire/shop.blade.php
@@ -1,3 +1,36 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+<div class="row">
+    <div class="col-md-9">
+        <div class="card shadow mb-3">
+            <div class="card-body">
+                <h2 class="font-weight-bold">Product List</h2> 
+                <div class="row">
+                    @foreach ($products as $a)
+                        <div class="col-md-3 mb-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="{{asset('storage/images/'.$a->image)}}" alt="Item" class="img-fluid" >
+                                </div>
+                                <div class="card-footer">
+                                    <h6 class="font-weight-bold text-center">{{$a->name}}</h6>
+                                   
+                                        <button class="btn btn-success btn-sm btn-block ">Add</button>
+                                         <button class="btn btn-primary btn-sm btn-block ">Detail</button>
+                                   
+                                   
+                                </div>
+                            </div>
+                        </div>   
+
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">  
+        <div class="card shadow mb-3">
+            <div class="card-body">
+                Cart
+            </div>
+        </div>
+    </div>
 </div>
